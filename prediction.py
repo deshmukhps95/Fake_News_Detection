dc96f2a4d69c149b3c8f333921efe14beb41759b
manedesh <prasad.maedeshmukh@tomtom.com>
2019-10-01 16:57:37 +0530

    code changes in prediction.py

--- prediction.py
+++ prediction.py
@@ -1,28 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec  4 17:45:40 2017
-
-@author: NishitP
-"""
-
 import pickle
+from preprocessing import process
+from feaure_extraction import FeatureExtractor
 
-#doc_new = ['obama is running for president in 2016']
-
-var = input("Please enter the news text you want to verify: ")
-print("You entered: " + str(var))
-
-
-#function to run for prediction
-def detecting_fake_news(var):    
-#retrieving the best model for prediction call
-    load_model = pickle.load(open('final_model.sav', 'rb'))
-    prediction = load_model.predict([var])
-    prob = load_model.predict_proba([var])
-
-    return (print("The given statement is ",prediction[0]),
-        print("The truth probability score is ",prob[0][1]))
+feature_extractor = FeatureExtractor()
 
 
-if __name__ == '__main__':
-    detecting_fake_news(var)
+def detecting_fake_news(model_checkpoint, sentence):
+    file = open(model_checkpoint, "rb")
+    model = pickle.load(file)
+    vectorizer = pickle.load(file)
+    features = vectorizer.transform([sentence]).toarray()
+    prediction = model.predict(features)
+    # probability = model.predict_proba([sentence])
+    print("The given statement is ", prediction[0])
+    # print("The truth probability score is ", probability[0][1])
+
+
+if __name__ == "__main__":
+    sentence = input("Please enter the news text you want to verify: ")
+    print("You entered: " + str(sentence))
+    pre_processed_sentence = process(sentence)
+    detecting_fake_news("models/fake_news_model.pkl", pre_processed_sentence)
